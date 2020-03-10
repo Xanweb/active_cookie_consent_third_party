3495a32bb4ffb5c55be85f29fc803b3001aa06b5
Hamed Darragi <hd@xanweb.com>
2020-03-10 12:02:41 +0100

    misspelling fix

--- config/cookie_disclaimer.php
+++ config/cookie_disclaimer.php
@@ -2,6 +2,6 @@
 return [
     'third_party' => [
         'title' => t('Third party'),
-        'description' => t('Third party services as youtube and google maps that uses cookies...'),
+        'description' => t('Third party services as youtube and google maps that use cookies...'),
     ]
 ];
