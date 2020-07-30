4b671dd184f18b9d1723a589cdbec36b4280a600
Hamed Darragi <hd@xanweb.com>
2020-07-30 11:47:14 +0100

    install npm packages and add webpack config

---
+++ build/webpack.mix.js
@@ -0,0 +1,30 @@
+const mix = require('laravel-mix');
+
+mix.webpackConfig({
+    resolve: {
+        symlinks: false
+    },
+    externals: {
+        jquery: 'jQuery',
+        vue: 'Vue',
+        cookie: 'Cookie'
+    }
+})
+
+mix.options({
+    processCssUrls: false
+})
+
+mix.setPublicPath('..')
+
+mix.sass('assets/sass/google-map.scss', 'css')
+    .js('assets/js/google-map.js', 'js')
+
+mix.sass('assets/sass/youtube.scss', 'css')
+    .js('assets/js/youtube.js', 'js')
+
+mix.disableNotifications()
+
+// Disable mix-manifest.json
+// @see https://github.com/JeffreyWay/laravel-mix/issues/580
+Mix.manifest.refresh = _ => void 0;
