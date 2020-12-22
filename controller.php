b7d6de6512cee5b1b3202385146151570c3f5c57
Hamed Darragi <hd@xanweb.com>
2020-12-22 22:32:01 +0100

    Release v1.3.0

--- controller.php
+++ controller.php
@@ -11,8 +11,8 @@ class Controller extends Package implements ProviderInterface
 {
     protected $pkgHandle = 'active_cookie_consent_third_party';
     protected $appVersionRequired = '8.5.1';
-    protected $pkgVersion = '1.3.0RC1';
-    protected $packageDependencies = ['active_cookie_consent' => '1.3.0RC1'];
+    protected $pkgVersion = '1.3.0';
+    protected $packageDependencies = ['active_cookie_consent' => '1.3.0'];
 
     public function getPackageDescription(): string
     {
