f855a1953d33b7a364768943087f546893e80c50
hamza <ha@xanweb.com>
2022-11-17 14:51:23 +0100

    ThirdParty Accept Button both action

--- build/assets/js/config.js
+++ build/assets/js/config.js
@@ -2,6 +2,7 @@ const defaults = {
     i18n: {
         third_party: {
             accept_btn: 'Please accept third party cookies',
+            show_popup_btn: 'More information',
             popup_msg: ''
         }
     },
@@ -28,12 +29,32 @@ export default class Config {
         return Config._get()._data.i18n.third_party.accept_btn
     }
 
+    static getShowPopupButtonText() {
+        return Config._get()._data.i18n.third_party.show_popup_btn
+    }
+
     static getAcceptButtonAction() {
         return Config._get()._data.third_party.accept_btn_action
     }
 
     static getAcceptButtonHTML() {
-        return `<button type="button" class="btn btn-info center-block display-cookies-disclaimer-popup" data-accept-function="${Config.getAcceptButtonAction()}">${Config.getAcceptButtonText()}</button>`
+        let buttonsHtml = ''
+        let acceptButtonAction = Config.getAcceptButtonAction()
+        if (acceptButtonAction === 'show_popup' || acceptButtonAction === 'both_action') {
+            buttonsHtml = buttonsHtml.concat(Config.getShowPopupButtonHTML())
+        }
+        if (acceptButtonAction === 'accept_third_party' || acceptButtonAction === 'both_action') {
+            buttonsHtml = buttonsHtml.concat(Config.getAcceptThirdPartyButtonHTML())
+        }
+        return buttonsHtml
+    }
+
+    static getShowPopupButtonHTML() {
+        return `<button type="button" class="btn btn-info center-block display-cookies-disclaimer-popup w-100 mb-2" data-accept-function="show_popup">${Config.getShowPopupButtonText()}</button>`
+    }
+
+    static getAcceptThirdPartyButtonHTML() {
+        return `<button type="button" class="btn btn-info center-block display-cookies-disclaimer-popup w-100 mb-2" data-accept-function="accept_third_party">${Config.getAcceptButtonText()}</button>`
     }
 
     static getOverlayHTML() {
