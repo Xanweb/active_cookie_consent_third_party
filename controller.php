020dcf4a01e58f4ad80711c06fde1da01e0c6d06
hamza <ha@xanweb.com>
2022-11-17 17:34:56 +0100

    Update package version

--- controller.php
+++ controller.php
@@ -11,7 +11,7 @@ class Controller extends Package implements ProviderInterface
 {
     protected $pkgHandle = 'active_cookie_consent_third_party';
     protected $appVersionRequired = '8.5.1';
-    protected $pkgVersion = '1.3.0';
+    protected $pkgVersion = '1.4.0';
     protected $packageDependencies = ['active_cookie_consent' => '1.3.0'];
 
     public function getPackageDescription(): string
